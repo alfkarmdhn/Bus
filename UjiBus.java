6238ea9fc898a82ab93674a4a3788c966043ed61
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 19:47:11 +0700

    Create UjiBus.java

---
+++ UjiBus.java
@@ -0,0 +1,23 @@
+
+package bus;
+
+public class UjiBus {
+            public static void main(String[] args) {
+        // TODO code application logic here
+        Bus busMini=new Bus(10);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(3);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+          
+    }
+}
+
+    
